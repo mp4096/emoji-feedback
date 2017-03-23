acbdf09504f34e60252a2c2d8eadfb9099efb057
Mikhail Pak <mikhail.pak@tum.de>
2017-03-23 10:11:33 +0100

    Generate an access token instead of using a user password

--- generate_auth.py
+++ generate_auth.py
@@ -1,21 +1,23 @@
-import codecs
-from getpass import getpass
 from hashlib import blake2s
 from secrets import token_bytes
-import sys
+
+TOKEN_SIZE = 16
 
 if __name__ == "__main__":
     # Generate salt
     salt = token_bytes(blake2s.SALT_SIZE)
-    # Initialise hasher object
+    # Generate access token
+    token = token_bytes(TOKEN_SIZE)
+    # Initialise a hasher object and hash the token
     h = blake2s(salt=salt)
+    h.update(token)
 
-    # Read and hash password
-    pswd = getpass(prompt="Input new password:")
-    h.update(pswd.encode(sys.stdin.encoding))
+    # Write the token to a file
+    with open("token", 'w') as f:
+        f.write("{:s}\n".format(token.hex()))
 
     # Write salt and hash to a TOML stub
-    with codecs.open("auth.toml", 'w', encoding="utf-8") as f:
+    with open("auth.toml", 'w') as f:
         f.write("[auth]\n")
         f.write('salt="{:s}"\n'.format(salt.hex()))
         f.write('hash="{:s}"\n'.format(h.digest().hex()))
